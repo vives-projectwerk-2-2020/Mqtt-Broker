e84e33481784f7b1d7590ead96c8d7c197c438da
rvis15 <r0790666@student.vives.be>
2020-03-20 14:12:15 +0100

    v0.1 broker working in frontend through websockets

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+var express = require('express')
+var app = express();
+const port = 3000;
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '\\views');
+
+
+var server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+  });
+
+app.route('/')
+.get(function(req, res){
+    res.render('index.ejs');
+});
+
+  
+server = app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
